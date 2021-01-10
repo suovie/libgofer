729864ab16998a9a7ba369818dedf7d06693b7ce
suovie <suovie@gmail.com>
2021-01-10 09:54:38 +0100

    declare message types

--- src/main/java/libgofer/Message.java
+++ src/main/java/libgofer/Message.java
@@ -8,6 +8,15 @@ public class Message implements Serializable {
     public String type, sender, content, recipient;
     public boolean encrypted = false;
 
+    public static final String MESSAGE_TYPE_MESSAGE = "MESSAGE";
+    public static final String MESSAGE_TYPE_CONNECT = "CONNECT";
+    public static final String MESSAGE_TYPE_WHOIS = "WHOIS";
+    public static final String MESSAGE_TYPE_LOGIN = "LOGIN";
+    public static final String MESSAGE_TYPE_SIGNUP = "SIGNUP";
+    public static final String MESSAGE_TYPE_NEW_USER = "NEWUSER";
+    public static final String MESSAGE_TYPE_UPLOAD = "UPLOAD";
+    public static final String MESSAGE_TYPE_DOWNLOAD = "DOWNLOAD";
+
     public Message(String t, String s, String c, String r) {
         this.type = t;
         this.sender = s;
